b5ba1d1a2b694ce9250a47e420a745a3574fb80e
sabretooth1405 <sabretooth1405@gmail.com>
2023-06-11 19:03:27 +0530

    deployment changes

--- quiz/quiz/settings.py
+++ quiz/quiz/settings.py
@@ -54,6 +54,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = "quiz.urls"
@@ -122,8 +123,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "quiz/static/"
-STATIC_ROOT= os.path.join(BASE_DIR, '/static')
+STATIC_URL = "static/"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS=[BASE_DIR/'static']
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
